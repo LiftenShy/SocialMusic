20c2984bcf56ae2c27590fe6fb7d074d8829022e
GOMEL\Siarhei_Privalau <Siarhei_Privalau@epam.com>
2018-06-07 13:03:32 +0300

    Init authorize ui

--- AuthorizeUI/Authorize/src/app/app.module.ts
+++ AuthorizeUI/Authorize/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
